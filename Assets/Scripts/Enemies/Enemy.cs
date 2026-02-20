922c21030f2fb44142835b082bcf8fc6428f3e7b
Ivan Perrusquia <joseivanr304@gmail.com>
2026-02-19 23:19:18 -0600

    UI Score funcionando y GameManager configurado

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -2,7 +2,13 @@
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpeed = 2f;
+    public Transform player;
+    public float attractionStrength = 0.4f;
+    public float detectionRange = 5f;
+
+    [Header("Freeze Settings")]
     public int hitsToFreeze = 3;
 
     private int currentHits = 0;
@@ -22,7 +28,30 @@ public class Enemy : MonoBehaviour
     {
         if (isFrozen) return;
 
-        rb.linearVelocity = new Vector2(direction * moveSpeed, rb.linearVelocity.y);
+        float movementX = direction;
+
+        if (player != null)
+        {
+            float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+            if (distanceToPlayer < detectionRange)
+            {
+                float directionToPlayer = Mathf.Sign(player.position.x - transform.position.x);
+                movementX += directionToPlayer * attractionStrength;
+            }
+        }
+
+        rb.linearVelocity = new Vector2(movementX * moveSpeed, rb.linearVelocity.y);
+
+        Flip();
+    }
+
+    void Flip()
+    {
+        if (rb.linearVelocity.x > 0)
+            sr.flipX = false;
+        else if (rb.linearVelocity.x < 0)
+            sr.flipX = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -30,7 +59,6 @@ public class Enemy : MonoBehaviour
         if (collision.gameObject.CompareTag("Wall"))
         {
             direction *= -1;
-            sr.flipX = !sr.flipX;
         }
     }
 
@@ -49,8 +77,35 @@ public class Enemy : MonoBehaviour
     void FreezeEnemy()
     {
         isFrozen = true;
+
         rb.linearVelocity = Vector2.zero;
-        rb.gravityScale = 0;
-        sr.color = Color.white;
+        rb.gravityScale = 1; // vuelve a tener gravedad
+        rb.freezeRotation = true;
+
+        sr.color = Color.cyan;
+
+        // Reduce fricción para que se deslice
+        PhysicsMaterial2D slippery = new PhysicsMaterial2D();
+        slippery.friction = 0f;
+        slippery.bounciness = 0f;
+
+        GetComponent<Collider2D>().sharedMaterial = slippery;
     }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!isFrozen) return;
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            float impactForce = rb.linearVelocity.magnitude;
+
+            if (impactForce > 3f)
+            {
+                GameManager.Instance.AddScore(100);
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+
 }
